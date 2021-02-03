da7dea23965a4904c7f104ccf5e06d4920635e14
MADU7513 <alexandre.devely@orange.com>
2021-02-03 11:51:36 +0100

    add log info

--- scripts/tag.dev2prod.sh
+++ scripts/tag.dev2prod.sh
@@ -4,8 +4,11 @@ corefiles=( "oc.pyos" "oc.nginx" "oc.mongo" "oc.user.18.04" "oc.pulseaudio.18.04
 
 for i in "${corefiles[@]}"
 do
+	echo "-> docker pull abcdesktopio/$i:dev"
 	docker pull abcdesktopio/$i:dev
+	echo "-> docker tag abcdesktopio/$i:dev abcdesktopio/$i"
  	docker tag abcdesktopio/$i:dev abcdesktopio/$i
+	echo "-> docker push abcdesktopio/$i"
 	docker push abcdesktopio/$i
 done
 
