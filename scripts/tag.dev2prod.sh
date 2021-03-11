530eecea524cf03326d3ef948a1b5aa8c85a48d9
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-03-11 09:19:47 +0100

    Update tag.dev2prod.sh

--- scripts/tag.dev2prod.sh
+++ scripts/tag.dev2prod.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 corefiles=( "oc.pyos" "oc.nginx" "oc.mongo" "oc.user.18.04" "oc.pulseaudio.18.04" "oc.cupsd.18.04" )
 
@@ -12,7 +12,7 @@ do
 	docker push abcdesktopio/$i
 done
 
-
+wget -O images-list.txt https://raw.githubusercontent.com/abcdesktopio/conf/main/images-list.txt
 while read i; do
   	docker pull abcdesktopio/$i:dev
         docker tag abcdesktopio/$i:dev abcdesktopio/$i
