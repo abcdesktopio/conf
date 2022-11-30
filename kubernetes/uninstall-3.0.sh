84a1821da8ae1437afe13515a791a1dc3b608755
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2022-11-30 16:37:22 +0100

    fix yaml file

--- kubernetes/uninstall-3.0.sh
+++ kubernetes/uninstall-3.0.sh
@@ -2,15 +2,15 @@ echo "starting abcdesktop uninstall commands"
 echo "stop and remove abcdesktop user pods"
 kubectl delete pods --selector="type=x11server" -n abcdesktop
 echo "remove all services, pods"
-kubectl delete -f https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/abcdesktop.yaml
+kubectl delete -f https://raw.githubusercontent.com/abcdesktopio/conf/main/kubernetes/abcdesktop-3.0.yaml 
 echo "remove all secrets"
 kubectl delete secrets --all -n abcdesktop
 echo "remove all configmaps"
 kubectl delete cm --all -n abcdesktop
 echo "remove all pvc"
-kubectl delete pvc --all -n abcdesktop
+kubectl delete pvc --all -n abcdesktop 2>/dev/null
 echo "remove all pv"
-kubectl delete pv --all -n abcdesktop
+kubectl delete pv --all -n abcdesktop  2>/dev/null
 echo "remove namespace"
 kubectl delete namespace abcdesktop
 echo "abcdesktop is uninstalled"
