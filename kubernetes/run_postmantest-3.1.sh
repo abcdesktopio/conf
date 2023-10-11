7a2a411b01e059c16b59e2c5a100a695311dc813
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-10-11 12:21:01 +0200

    change debug stdout

--- kubernetes/run_postmantest-3.1.sh
+++ kubernetes/run_postmantest-3.1.sh
@@ -3,4 +3,6 @@
 NGINX_POD=$(kubectl get pods -l run=nginx-od  --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -n abcdesktop)
 echo "nginx pod name is $NGINX_POD"
 # run test
-kubectl debug -i -n abcdesktop $NGINX_POD --image=abcdesktopio/oc.postmantest:main -- /docker-entrypoint.sh
+kubectl debug -i -n abcdesktop $NGINX_POD --image=abcdesktopio/oc.postmantest:main -- /docker-entrypoint.sh > a
+echo "dump stdout"
+cat a
