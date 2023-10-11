9ad2278d7a28891db0d90cccee579fc0241420d1
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-10-11 21:03:45 +0200

    fix debug

--- kubernetes/run_postmantest-3.1.sh
+++ kubernetes/run_postmantest-3.1.sh
@@ -3,4 +3,4 @@
 NGINX_POD=$(kubectl get pods -l run=nginx-od  --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -n abcdesktop)
 echo "nginx pod name is $NGINX_POD"
 # run test
-kubectl debug -i -n abcdesktop $NGINX_POD --image=abcdesktopio/oc.postmantest:main -- /docker-entrypoint.sh
+kubectl debug -it -n abcdesktop $NGINX_POD --image=abcdesktopio/oc.postmantest:main -- /docker-entrypoint.sh
