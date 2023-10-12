458bfdedb4ce5f06f282d28f5ab70b490127610a
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-10-12 16:00:16 +0200

    add log debug

--- kubernetes/samples/create_pod.sh
+++ kubernetes/samples/create_pod.sh
@@ -7,14 +7,15 @@ json_login=$(curl -X POST $ABCDESKTOP_URL/API/auth/auth  -H 'Content-Type: appli
 jwt_user_token=$(echo $json_login|jq -r .result.jwt_user_token)
 abcauthorization="Bearer $jwt_user_token"
 #echo "HEADER=$abcauthorization"
-
+echo "json_login=$json_login"
 # launch a pod 
 desktop_pod=$(curl -X POST $ABCDESKTOP_URL/API/composer/launchdesktop -H 'Content-Type: application/json' -H "Abcauthorization: $abcauthorization"  -d '{}')
+echo "desktop_pod=$desktop_pod"
 
 # logout
 logout=$(curl -X POST $ABCDESKTOP_URL/API/auth/logout -H 'Content-Type: application/json' -H "Abcauthorization: $abcauthorization"  -d '{}')
 # echo "logout=$logout"
+echo "logout=$logout"
 
-echo "$desktop_pod"
 
 
